40ae0ef2053028a266db3385dfd11fb3a453340a
karadenizemirr <karadenizemirr@gmail.com>
2023-08-01 02:07:31 +0300

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
   });
 
   await app.register(fastifyCsrf);
-  //app.useGlobalFilters(new GlobalExceptionFilter())
+  app.useGlobalFilters(new GlobalExceptionFilter())
 
   await app.listen(process.env.PORT ?? 3000, process.env.HOST || '0.0.0.0');
 }
